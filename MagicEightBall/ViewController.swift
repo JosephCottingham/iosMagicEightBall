793d4693a2d8cb2105693ca76cc618da7c644656
Joseph Cottingham <joseph.h.cottingham@gmail.com>
2020-11-20 13:49:57 -0500

    init

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
+    @IBOutlet weak var shakeItBtn: UIButton!
+    @IBOutlet weak var answerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func shakeBtnPress(_ sender: Any){
+        print("Shake It!")
+        answerLabel.text = answers.randomElement()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        answerLabel.text = answers.randomElement()
+    }
 
 }
 
